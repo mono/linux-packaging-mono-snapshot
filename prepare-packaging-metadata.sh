5b12b62f558e921d92e89e7214f3f0576509d161
Jo Shields <directhex@apebox.org>
2012-07-01 11:41:32 +0100

    Fix final tar command

--- prepare-packaging-metadata.sh
+++ prepare-packaging-metadata.sh
@@ -44,4 +44,4 @@ mkdir -p ${PACKAGING_ROOT}/temp
 tar xf mono*tar* -C ${PACKAGING_ROOT}/temp
 mv ${PACKAGING_ROOT}/temp/mono* ${PACKAGING_ROOT}/temp/mono-snapshot-${TIMESTAMP}
 mv debian ${PACKAGING_ROOT}/temp/mono-snapshot-${TIMESTAMP}
-cd ${PACKAGING_ROOT}/temp/ && tar cjvf mono-snapshot-${TIMESTAMP}_${TIMESTAMP}.tar.bz2 && cd $MONO_ROOT
+cd ${PACKAGING_ROOT}/temp/ && tar cjvf mono-snapshot-${TIMESTAMP}_${TIMESTAMP}.tar.bz2 mono-snapshot-${TIMESTAMP} && cd $MONO_ROOT
