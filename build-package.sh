48e55eb9a27d794f517def849dcf574f4fb8fb20
Jo Shields <directhex@apebox.org>
2012-07-17 23:43:55 +0100

    [debian] Add a tiny script to build the package in the temp/ folder, as created by prepare-packaging-metadata.sh

---
+++ build-package.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# amazeballs.sh
+# Copyright 2012 Jo Shields
+#
+# because mono snapshots on debian are totes amazeballs
+#
+# Licensed under WTFPLv2
+
+
+PACKAGING_ROOT="$( cd "$( dirname "$0" )" && pwd )"
+BUILD_ARCH=$(dpkg-architecture -qDEB_BUILD_ARCH)
+DEB_BUILD_OPTS="-us -uc"
+if [ "${BUILD_ARCH}" != "amd64" ]
+then
+	DEB_BUILD_OPTS="-B ${DEB_BUILD_OPTS}"
+fi
+
+echo "Building package in ${PACKAGING_ROOT}"
+cd ${PACKAGING_ROOT}/temp/* && dpkg-buildpackage ${DEB_BUILD_OPTS}
